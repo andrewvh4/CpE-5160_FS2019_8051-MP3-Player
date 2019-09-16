d822cef29c8699d1ad5eb425c545a5ecf57032d8
Andrew Van Horn <andrewvh7@gmail.com>
2019-09-15 20:39:03 -0500

    Added UART libraries

---
+++ MP3_Player_Software/Experiment 1/src/Drivers/UART.c
@@ -0,0 +1,26 @@
+#include "../application/main.h"
+#include "UART.h"
+
+//Calculate baud rate generator values for BaudRate 9600
+
+uint8_t UART_Init_9600()
+{
+	//Initialize hardware registers (PCON, SCON, BDRCON)
+	
+	//Disable UART interrupts
+	
+	//Set TI bit 
+	
+	
+	return 1;
+}
+
+uint8_t UART_Transmit(uint8_t character)
+{
+	return 1;
+}
+
+uint8_t UART_Receive()
+{
+	return 1;
+}
