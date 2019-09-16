ab37312cd2f55ae9dcd684725f2367194a98c9d5
Andrew Van Horn <andrewvh7@gmail.com>
2019-09-15 20:39:21 -0500

    added main file

---
+++ MP3_Player_Software/Experiment 1/src/Application/Main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "../Drivers/PORT.H"
+#include <stdio.h>
+#include "../Drivers/UART.h"
+
+uint8_t setup();
+uint8_t loop();
+
+
+void main()
+{
+	setup();
+	while(1)
+	{
+		loop();
+	}
+}
+
+uint8_t setup()
+{
+	//Serial Init
+	return(1)
+}
+
+uint8_t loop()
+{
+	
+	return(1)
+}
+
