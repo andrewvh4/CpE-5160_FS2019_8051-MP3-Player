c8e28486b267f211fdad02bbf84bf1d6e85a8a47
Andrew Van Horn <andrewvh7@gmail.com>
2019-09-19 11:17:54 -0500

    fixed compile errors on LCD functions

--- MP3_Player_Software/Experiment 1/src/Modules/LCD.c
+++ MP3_Player_Software/Experiment 1/src/Modules/LCD.c
@@ -4,22 +4,28 @@
 uint8_t LCD_Write_String(uint8_t* string, uint8_t line, uint8_t starting_char)
 {
 	//Call Write Data
+	return(1);
 }
 
 uint8_t LCD_Init()
 {
+	return(1);
 }
 
 uint8_t LCD_Write_Data(uint8_t dat)
 {
+	return(1);
 }
-uint8_t LCD_Write_Data(uint8_t dat, uint8_t rs_value)
+uint8_t LCD_Write_DataRS(uint8_t dat, uint8_t rs_value)
 {
+	return(1);
 }
 
 uint8_t LCD_Write_Command(uint8_t command)
 {
+	return(1);
 }
-uint8_t LCD_Write_Command(uint8_t command, uint8_t rs_value)
+uint8_t LCD_Write_CommandRS(uint8_t command, uint8_t rs_value)
 {
+
 }
--- MP3_Player_Software/Experiment 1/src/Modules/LCD.h
+++ MP3_Player_Software/Experiment 1/src/Modules/LCD.h
@@ -23,7 +23,9 @@ uint8_t LCD_Write_String(uint8_t* string, uint8_t line, uint8_t starting_char);
 uint8_t LCD_Init();
 
 uint8_t LCD_Write_Data(uint8_t dat);
-uint8_t LCD_Write_Data(uint8_t dat, uint8_t rs_value);
+uint8_t LCD_Write_DataRS(uint8_t dat, uint8_t rs_value);
+uint8_t LCD_Write_Data(uint8_t dat);
+uint8_t LCD_Write_DataRS(uint8_t dat, uint8_t rs_value);
 
 uint8_t LCD_Write_Command(uint8_t command);
 uint8_t LCD_Write_Command(uint8_t command, uint8_t rs_value);
