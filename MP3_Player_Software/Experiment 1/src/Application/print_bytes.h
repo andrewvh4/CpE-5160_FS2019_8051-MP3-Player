b500ed1b801c79f70ba333dc4084977b846ed67f
Andrew Van Horn <andrewvh7@gmail.com>
2019-09-22 18:35:05 -0500

    Added constants to printBytes

--- MP3_Player_Software/Experiment 1/src/Application/print_bytes.h
+++ MP3_Player_Software/Experiment 1/src/Application/print_bytes.h
@@ -14,5 +14,11 @@ void print_16bytes(uint8_t * array_in);
 
 void print_memory(uint8_t * array_in, uint16_t number_of_bytes);
 
+#define space (0x20)
+#define CR (0x0D)
+#define LF (0x0A)
+#define BS (0x08)
+#define DEL (0x7F)
+
 
 #endif
