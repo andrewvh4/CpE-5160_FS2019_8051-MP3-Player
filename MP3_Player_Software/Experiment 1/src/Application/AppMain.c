bb72dda40f5bcc46cedbee0606eeee439663878d
Andrew Van Horn <andrewvh7@gmail.com>
2019-09-22 20:19:10 -0500

    Memory Test working

--- MP3_Player_Software/Experiment 1/src/Application/AppMain.c
+++ MP3_Player_Software/Experiment 1/src/Application/AppMain.c
@@ -4,6 +4,9 @@
 #include "../Drivers/UART.h"
 #include "../Drivers/Timing.h"
 
+#include "../Drivers/memory_test.h"
+#include "print_bytes.h"
+
 uint8_t setup();
 uint8_t loop();
 
@@ -19,21 +22,26 @@ void main()
 
 uint8_t setup()
 {
+	Port_writePin(GREEN_LED, LOW);
+	
+	
 	//Serial Init
 	UART_Init_9600();
 	
-	Port_writePin(GREEN_LED, LOW);
 	Timing_delay_ms(1000);
 	Port_writePin(GREEN_LED, HIGH);
-	
 	return(1);
 }
 
 uint8_t loop()
 {
-	printf("Hello World\n");
-	Timing_delay_ms(1000);
+	uint8_t* generic_pointer;
 	
+	generic_pointer = xdata_memory_init();
+	
+	print_memory(generic_pointer, 100);
+	
+	Timing_delay_ms(1000);
 	return(1);
 }
 
