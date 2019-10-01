39652b28073733ee4dcb5072350b8750a3cfc1aa
Andrew Van Horn <andrewvh7@gmail.com>
2019-10-01 13:02:05 -0500

    Added appMain comments

--- MP3_Player_Software/Experiment 2/src/Application/AppMain.c
+++ MP3_Player_Software/Experiment 2/src/Application/AppMain.c
@@ -23,16 +23,21 @@ void main()
 
 uint8_t setup()
 {
-	//Set XRAM Size
+	//Set XRAM Size 1024
+	//Setup UART
+	//Setup SPI with clockrate 400k
+	//Setup SD
+	//Posibly set up PSI with clockrate of 25M
 	
-	
-	return(1);
+	return(0);
 }
 
 uint8_t loop()
 {
-	
+	//Prompt user to enter a block number to be read
+	//Read a block
+	//Print memory
 
-	return(1);
+	return(0);
 }
 
